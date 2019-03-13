632b5c599d55eaeb368d6fd87c89c0ed39ef3b0a
Eldar Oralov <eldar.oralov@gmail.com>
2019-03-13 17:37:44 +0600

    Second Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train train1 = new Train();
+            train1.GetInfo();
+
+            int numberOfTrain = 5;
+
+            Train[] train2 = new Train[numberOfTrain];
+            for(int i =0;i< numberOfTrain; i++)
+            {
+                train2[i] = new Train
+                {
+                    Name = "123asd",
+                    TypeOfTrain = Type.Cargo,
+                    NumberOfSeat = i+10,
+                    IsOnTrip = true
+                };
+            }
+            foreach(Train tr in train2)
+            {
+                tr.GetInfo();
+            }
+
+            train1.DoRing();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Train.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Type { Unknown, Passengers, Cargo } 
+    public partial class Train
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+        private Type _typeOfTrain;
+        public Type TypeOfTrain { get { return _typeOfTrain; } set { _typeOfTrain = value; } }
+        private int _numberOfSeat;
+        public int NumberOfSeat { get { return _numberOfSeat; } set { _numberOfSeat = value; } }
+        private bool _isOnTrip;
+        public bool IsOnTrip { get { return _isOnTrip; } set { _isOnTrip = value; } }
+        static Train()
+        {
+            Console.WriteLine("New train(s) created");
+            Console.Write("\a");
+        }
+        public Train()
+        {
+            _name = "No name";
+            _typeOfTrain = Type.Unknown;
+            _numberOfSeat = 0;
+            _isOnTrip = false;
+        }
+        public Train(string name, Type type, int number, bool trip)
+        {
+            _name = name;
+            _typeOfTrain = type;
+            _numberOfSeat = number;
+            _isOnTrip = trip;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name of train: {_name}\nType of train: {(Type)_typeOfTrain}\nNumber of seats: {_numberOfSeat}\nOn trip? {_isOnTrip}");
+        }
+        partial void Ring();
+        public void DoRing()
+        {
+            Ring();
+        }
+    }
+}
